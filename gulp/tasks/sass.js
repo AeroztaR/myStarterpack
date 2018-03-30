8a1696426b92e3f2a9abc7b533a513170ecda8c3
AeroztaR <jorguinhos89@gmail.com>
2018-03-30 22:06:20 +0500

    first commit

---
+++ gulp/tasks/sass.js
@@ -0,0 +1,18 @@
+module.exports = () => {
+    $.gulp.task('sass', () => {
+        return $.gulp.src('src/static/sass/main.scss')
+          .pipe($.plugins.sourcemaps.init())
+          .pipe($.plugins.sass())
+          .pipe($.plugins.cssimport())
+          .pipe($.plugins.autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+          }))
+          .pipe($.plugins.csso())
+          .pipe($.plugins.sourcemaps.write())
+          .pipe($.gulp.dest('build/static/css/'))
+          .pipe($.browserSync.reload({
+              stream: true
+          }));
+      });
+};
