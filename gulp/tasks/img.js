b57ddcc969fca5615865ca0421e967eb38b83089
AeroztaR <jorguinhos89@gmail.com>
2021-04-11 04:47:21 +0500

    Added mozjpeg option in img task

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,27 +1,30 @@
 module.exports = () => {
-    $.gulp.task('img:dev', () => {
-        return $.gulp.src('src/static/img/**/*')
-        .pipe($.gulp.dest('build/static/img'));
-    });
+  $.gulp.task('img:dev', () => {
+    return $.gulp
+      .src('src/static/img/**/*')
+      .pipe($.gulp.dest('build/static/img'));
+  });
 
-    $.gulp.task('img:build', () => {
-        return $.gulp.src('src/static/img/**/*')
-        .pipe($.plugins.imagemin([
-            $.plugins.imagemin.gifsicle({interlaced: true}),
-            $.plugins.imagemin.jpegtran({progressive: true}),
-            $.plugins.imagemin.optipng({optimizationLevel: 3}),
+  $.gulp.task('img:build', () => {
+    return (
+      $.gulp
+        .src('src/static/img/**/*')
+        .pipe(
+          $.plugins.imagemin([
+            $.plugins.imagemin.gifsicle({ interlaced: true }),
+            $.plugins.imagemin.mozjpeg({ progressive: true }),
+            $.plugins.imagemin.optipng({ optimizationLevel: 3 }),
             $.plugins.imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
-        ]))
+              plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+            }),
+          ])
+        )
         // .pipe($.plugins.webp({
         //     use: [
         //         $.plugins.webp({quality: 90})
         //     ]
         // }))
-        .pipe($.gulp.dest('build/static/img'));
-    });
+        .pipe($.gulp.dest('build/static/img'))
+    );
+  });
 };
